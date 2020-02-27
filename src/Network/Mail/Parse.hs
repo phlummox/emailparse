94fb87017dfa07d5a44af53e8a834c474640023d
phlummox <phlummox@localhost>
2020-02-27 08:14:37 +0000

    add more useful exports to Network.Mail.Parse

--- src/Network/Mail/Parse.hs
+++ src/Network/Mail/Parse.hs
@@ -1,6 +1,13 @@
-module Network.Mail.Parse (parseMessage) where
+module Network.Mail.Parse
+  (
+    parseMessage
+  , module Network.Mail.Parse.Types
+  , module Network.Mail.Parse.Utils
+  )
+  where
 
 import Network.Mail.Parse.Types
+import Network.Mail.Parse.Utils
 import Network.Mail.Parse.Parsers.Message (messageParser)
 
 import qualified Data.ByteString.Char8 as BSC
